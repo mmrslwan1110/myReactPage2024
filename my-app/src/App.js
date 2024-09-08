baa5c04ed7a8bc98e84b6d6805901d555af7cda6
mmrslwan1110 <kp@mmrsl.cn>
2024-09-08 17:03:59 +0800

    study react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { UserOutlined } from '@ant-design/icons';
-import { Card, Form, Input, Button } from 'antd'
+import Login from './pages/Login'
+
 function App() {
   return (
-    <div className="login">
-    <Card className="login-container">
-      <img className="login-logo" src={logo} alt="" />
-      {/* 登录表单 */}
-      <Form>
-        <Form.Item>
-          <Input size="large" placeholder="请输入手机号" />
-        </Form.Item>
-        <Form.Item>
-          <Input size="large" placeholder="请输入验证码" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" size="large" block>
-            登录
-          </Button>
-        </Form.Item>
-      </Form>
-    </Card>
-  </div>
-  )
+    <Login/>
+    );
 }
-
-
-
-
 
 export default App;
---
+++ my-app/src/pages/Login/index.js
@@ -0,0 +1,49 @@
+import './index.css'
+import { Card, Form, Input, Button, message } from 'antd'
+import logo from './logo.svg';
+
+const Login = () => {
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        {/* onFinish={onFinish} */}
+        <Form  validateTrigger="onBlur">
+          <Form.Item
+            name="mobile" //指定校验字段名
+            // 多条校验逻辑 先校验第一条 第一条通过之后再校验第二条
+            rules={[
+              {
+                required: true,
+                message: '请输入手机号',
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '请输入正确的手机号格式'
+              }
+            ]}>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: '请输入验证码',
+              },
+            ]}>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
+}
+
+export default Login
