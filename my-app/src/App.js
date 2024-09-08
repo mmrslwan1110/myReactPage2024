0de45d9f74989ce101cb1d55362f3c76d429acab
mmrslwan1110 <kp@mmrsl.cn>
2024-09-08 16:25:47 +0800

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { UserOutlined } from '@ant-design/icons';
+import { Card, Form, Input, Button } from 'antd'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="login">
+    <Card className="login-container">
+      <img className="login-logo" src={logo} alt="" />
+      {/* 登录表单 */}
+      <Form>
+        <Form.Item>
+          <Input size="large" placeholder="请输入手机号" />
+        </Form.Item>
+        <Form.Item>
+          <Input size="large" placeholder="请输入验证码" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" size="large" block>
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  </div>
+  )
 }
+
+
+
+
 
 export default App;
